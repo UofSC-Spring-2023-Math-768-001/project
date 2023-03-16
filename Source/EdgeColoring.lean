69d534c7b007d78638ed9cb6571cffda920d3e22
Matthew Ballard <matt@mrb.email>
2023-03-16 16:03:27 -0400

    add edge coloring

--- Source/EdgeColoring.lean
+++ Source/EdgeColoring.lean
@@ -1,5 +1,26 @@
 import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Coloring 
 
-#check SimpleGraph.Coloring 
+#check SimpleGraph.Coloring
 
+universe u w
+
+open SimpleGraph
+
+variable {V : Type u} {G : SimpleGraph V}
+
+def AdjacentAt (v : V) (e₁ e₂ : edgeSet G) : Prop := Sym2.Mem v e₁ ∧ Sym2.Mem v e₂
+
+theorem AdjacentAt.symm {v : V} {e₁ e₂ : edgeSet G} (h : AdjacentAt v e₁ e₂) : AdjacentAt v e₂ e₁ := ⟨h.right, h.left⟩
+
+def Adjacent (e₁ e₂ : edgeSet G) : Prop := ∃ v, AdjacentAt v e₁ e₂
+
+@[symm, aesop unsafe 10% apply (rule_sets [SimpleGraph])]
+theorem Adjacent.symm {e₁ e₂ : edgeSet G} (h : Adjacent e₁ e₂) : Adjacent e₂ e₁ := by 
+  obtain ⟨w,h'⟩ := h
+  exact ⟨w,AdjacentAt.symm h'⟩ 
+
+def lineGraph (G : SimpleGraph V) : SimpleGraph (edgeSet G) where 
+  Adj := fun e₁ e₂ => Adjacent e₁ e₂ ∧ e₁ ≠ e₂ 
+
+abbrev EdgeColoring (α : Type) := Coloring (lineGraph G) α 
