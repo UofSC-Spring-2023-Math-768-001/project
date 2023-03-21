ae98e01d4fdfbf92d1fe259b1dcccea0d387218e
Matthew Ballard <matt@mrb.email>
2023-03-21 16:06:41 -0400

    add neighborhood subgraph

--- Source/EdgeColoring.lean
+++ Source/EdgeColoring.lean
@@ -1,13 +1,18 @@
-import Mathlib.Combinatorics.SimpleGraph.Basic
 import Mathlib.Combinatorics.SimpleGraph.Coloring 
-
-#check SimpleGraph.Coloring
 
 universe u w
 
 open SimpleGraph
 
-variable {V : Type u} {G : SimpleGraph V}
+variable {V : Type u} {G : SimpleGraph V} {α : Type v}
+
+def closedNbhdSubgraph (v : V) : SimpleGraph {w | G.Adj v w ∨ w = v} where 
+  Adj := fun w₁ w₂ => G.Adj w₁ w₂ ∧ (w₁ = v ∨ w₂ = v)
+  symm := by
+    intro v₁ v₂ h
+    constructor
+    · apply G.symm <| h.left
+    · sorry
 
 def AdjacentAt (v : V) (e₁ e₂ : edgeSet G) : Prop := Sym2.Mem v e₁ ∧ Sym2.Mem v e₂
 
@@ -23,4 +28,29 @@ theorem Adjacent.symm {e₁ e₂ : edgeSet G} (h : Adjacent e₁ e₂) : Adjacen
 def lineGraph (G : SimpleGraph V) : SimpleGraph (edgeSet G) where 
   Adj := fun e₁ e₂ => Adjacent e₁ e₂ ∧ e₁ ≠ e₂ 
 
-abbrev EdgeColoring (α : Type) := Coloring (lineGraph G) α 
+variable (G)
+
+abbrev EdgeColoring (α : Type v) := Coloring (lineGraph G) α
+
+theorem EdgeColoring.valid {α : Type v} (G : SimpleGraph V)
+    (c : EdgeColoring G α) {e₁ e₂ : edgeSet G} (h : e₁ ≠ e₂) 
+    (adj : Adjacent e₁ e₂ ) : c e₁ ≠ c e₂ :=
+  Coloring.valid c ⟨adj,h⟩
+
+noncomputable def edgeChromaticNumber : ℕ := chromaticNumber (lineGraph G)
+
+variable (v : V) [Fintype (G.neighborSet v)] 
+
+def restrictedColoring (c : EdgeColoring G α) : G.neighborSet v → α := sorry
+
+theorem ge_degree_of_coloring (n : ℕ) (c : EdgeColoring G (Fin n)) : 
+    n ≥ G.degree v := by
+  by_contra h
+  sorry
+
+theorem ge_degree_of_colorable (n : ℕ) (h : G.Colorable n) : 
+    n ≥ G.degree v := sorry
+
+theorem edgeChromaticNumber_ge_degree :
+    edgeChromaticNumber G ≥ G.degree v := by 
+  sorry
